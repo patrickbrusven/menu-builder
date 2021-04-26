e8b68166ac52a41fda858da1339d3f7ec738b969
Patrick Brusven <pjbrusven@gmail.com>
2021-04-26 16:27:19 -0700

    updated backend index.js to point to routes folder.

--- server/index.js
+++ server/index.js
@@ -4,21 +4,10 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv/config');
 
-const app = express();
-
-// Middleware for dependencies
-
-app.use(bodyParser.json());
-app.use(cors());
-
-// routes
-app.get('/', (req, res) => {
-  res.send('this is a home page????');
-});
-
 // connect to db
 mongoose.connect(process.env.DB_CONNECTION, {
   useUnifiedTopology: true,
+  useFindAndModify: false,
   useNewUrlParser: true
 }).then(() => {
     console.log('connected to db')
@@ -27,6 +16,19 @@ mongoose.connect(process.env.DB_CONNECTION, {
     console.log("Database cound't be connected to: " + error)
   }
 )
+
+// routes
+const menu = require('./routes/menuAPI.js');
+
+const app = express();
+
+// Middleware for dependencies
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/api/menu', menu);
+
+
 
 const port = process.env.PORT || 5000;
 
