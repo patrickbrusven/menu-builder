cd6145170cc4728d206093adbc411255614c636f
Patrick Brusven <pjbrusven@gmail.com>
2021-05-14 09:01:31 -0700

    Moved Navigation to its own component.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/login">Login</router-link> |
-    <router-link to="/dashboard">Dashboard</router-link> |
-    <router-link to="/register">Register</router-link>
-  </div>
+  <Navigation />
   <router-view/>
 </template>
+
+<script>
+import Navigation from '@/components/Navigation.vue'
+
+export default {
+
+  components: {
+    Navigation,
+  }
+}
+</script>
 
 <style>
 #app {
@@ -16,18 +21,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/login">Login</router-link> |
+    <router-link to="/dashboard">Dashboard</router-link> |
+    <router-link to="/register">Register</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+}
+</script>
+
+<style scoped>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
